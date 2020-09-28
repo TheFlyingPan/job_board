f46fdc061087a7381ec10153f3b1787017997716
Pierre <bertrand.bouteloup@epitech.eu>
2020-09-28 15:42:33 +0200

    ADD: Routes GET

--- Step 04/back/index.js
+++ Step 04/back/index.js
@@ -10,30 +10,70 @@ var con = mysql.createConnection({
     database: "job_board"
 });
 
-app.get('/ad/:id', (req, res) => {
-    var sql = "SELECT * FROM advertisements WHERE id=" + req.params.id;
-    var query;
-    con.query(sql, function(err, result) {
-        if (err) throw err;
-        query = result;
-        console.log(result)
-        res.send(JSON.stringify(query));
-    });
-})
-
 app.get('/ad/', (req, res) => {
     var sql = "SELECT * FROM advertisements";
-    var query;
     con.query(sql, function(err, result) {
         if (err) throw err;
-        query = result;
-        console.log(result);
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.send(JSON.stringify(query));
+        res.send(JSON.stringify(result));
+    });
+})
+
+app.get('/ad/:id', (req, res) => {
+    var sql = "SELECT * FROM advertisements WHERE id=" + req.params.id;
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        res.send(JSON.stringify(result));
+    });
+})
+
+app.get('/companies/', (req, res) => {
+    var sql = "SELECT * FROM companies";
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        res.send(JSON.stringify(result));
+    });
+})
+
+app.get('/company/:id', (req, res) => {
+    var sql = "SELECT * FROM companies WHERE id=" + req.params.id;
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        res.send(JSON.stringify(result));
     });
 })
 
 
+app.get('/applications/', (req, res) => {
+    var sql = "SELECT * FROM application";
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        res.send(JSON.stringify(result));
+    });
+})
+
+app.get('/application/:id', (req, res) => {
+    var sql = "SELECT * FROM application WHERE id=" + req.params.id;
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        res.send(JSON.stringify(result));
+    });
+})
+
+app.get('/people/', (req, res) => {
+    var sql = "SELECT * FROM people";
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        res.send(JSON.stringify(result));
+    });
+})
+
+app.get('/people/:id', (req, res) => {
+    var sql = "SELECT * FROM people WHERE id=" + req.params.id;
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        res.send(JSON.stringify(result));
+    });
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
