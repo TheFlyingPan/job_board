83e76d52c0ff194d4576627e4600cdfff092f946
Pierre <bertrand.bouteloup@epitech.eu>
2020-09-28 16:45:26 +0200

    UPDATE: Database : Date

--- Step 01/job_board.sql
+++ Step 01/job_board.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 28, 2020 at 08:03 AM
--- Server version: 10.4.10-MariaDB
--- PHP Version: 7.3.12
+-- Generation Time: Sep 28, 2020 at 02:32 PM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -35,11 +34,18 @@ CREATE TABLE IF NOT EXISTS `advertisements` (
   `author` int(11) NOT NULL,
   `content` text NOT NULL,
   `company` int(11) NOT NULL,
-  `createdAt` date NOT NULL,
-  `beginsAt` date NOT NULL,
-  `expiresAt` date NOT NULL,
+  `createdAt` int(11) NOT NULL,
+  `beginsAt` int(11) NOT NULL,
+  `expiresAt` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `advertisements`
+--
+
+INSERT INTO `advertisements` (`id`, `title`, `author`, `content`, `company`, `createdAt`, `beginsAt`, `expiresAt`) VALUES
+(2, 'Job de ouf', 1, 'Content de lannonce du job de ouf', 45, 28092020, 30092020, 15102020);
 
 -- --------------------------------------------------------
 
@@ -55,7 +61,14 @@ CREATE TABLE IF NOT EXISTS `application` (
   `company_id` int(11) NOT NULL,
   `advertisement_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `application`
+--
+
+INSERT INTO `application` (`id`, `candidate_id`, `contact_id`, `company_id`, `advertisement_id`) VALUES
+(1, 2, 2, 3, 2);
 
 -- --------------------------------------------------------
 
@@ -71,7 +84,14 @@ CREATE TABLE IF NOT EXISTS `companies` (
   `country` varchar(255) NOT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `companies`
+--
+
+INSERT INTO `companies` (`id`, `name`, `city`, `country`, `description`) VALUES
+(1, 'Tribu', 'Ici', 'Dans le coin', 'La tribu d\'ici');
 
 -- --------------------------------------------------------
 
@@ -87,7 +107,7 @@ CREATE TABLE IF NOT EXISTS `people` (
   `applying` tinyint(1) NOT NULL,
   `lastName` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
